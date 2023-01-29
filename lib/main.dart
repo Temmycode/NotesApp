23ceb385e26abd8cf101d3c11347cc325e5dad0a
Temmycode <tolutech2004@gmail.com>
2023-01-29 23:19:04 +0100

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,19 @@
 import 'package:animation_and_notes/firebase_options.dart';
-import 'package:animation_and_notes/helper/helper_function.dart';
 import 'package:animation_and_notes/screens/home_screen.dart';
-import 'package:animation_and_notes/screens/introductions/introduction_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart' show Firebase;
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late bool isFirstTime = true;
-  getFirstTime() async {
-    await HelperFunction.getUserIsFirstTime().then((value) {
-      setState(() {
-        isFirstTime = value!;
-      });
-    });
-  }
-
-  @override
-  void initState() {
-    getFirstTime();
-    super.initState();
-  }
 
   // This widget is the root of your application.
   @override
@@ -43,7 +24,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: isFirstTime ? const IntroductionScreen() : const HomePage(),
+      home: const HomePage(),
     );
   }
 }
